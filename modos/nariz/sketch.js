8c5bb3ae5c7f8fb76976c2ba8501b58f293f6a2a
Maximo Signiorini <maximosigniorini97@gmail.com>
2020-07-31 16:17:42 -0300

    Code cleaning

--- modos/nariz/sketch.js
+++ modos/nariz/sketch.js
@@ -1,4 +1,3 @@
-var socket;
 let video;
 let poseNet;
 let poses = [];
@@ -77,34 +76,6 @@ function draw() {
   drawKeypoints();
 }
 
-
-function setupOsc(oscPortIn, oscPortOut) {
-  socket = io.connect('http://127.0.0.1:8081', {
-    port: 8081,
-    rememberTransport: false
-  });
-  socket.on('connect', function() {
-    socket.emit('config', {
-      server: {
-        port: oscPortIn,
-        host: '127.0.0.1'
-      },
-      client: {
-        port: oscPortOut,
-        host: '127.0.0.1'
-      }
-    });
-  });
-  socket.on('message', function(msg) {
-    if (msg[0] == '#bundle') {
-      for (var i = 2; i < msg.length; i++) {
-        receiveOsc(msg[i][0], msg[i].splice(1));
-      }
-    } else {
-      receiveOsc(msg[0], msg.splice(1));
-    }
-  });
-}
 
 function modelReady() {
   console.log('model ready');
