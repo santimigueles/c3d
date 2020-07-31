46e91fa047570fdabf208b3b1a5e7267b329fe73
Maximo Signiorini <maximosigniorini97@gmail.com>
2020-07-31 15:04:13 -0300

    Modo basico osc funcando

---
+++ modos/osc/sketch.js
@@ -0,0 +1,99 @@
+var socket;
+let video;
+let poseNet;
+let poses = [];
+let poseX, poseY;
+
+function setup() {
+  createCanvas(640, 480)
+
+  setupOsc(12000, 3334);
+
+  video = createCapture(VIDEO);
+  video.size(width, height)
+  video.hide();
+
+  poseNet = ml5.poseNet(video, modelReady);
+  poseNet.on('pose', function(results) {
+    poses = results;
+  });
+}
+
+function draw() {
+  translate(video.width, 0);
+  scale(-1, 1);
+  image(video, 0, 0, 640, 480);
+  drawKeypoints();
+  drawSkeleton();
+}
+
+
+function setupOsc(oscPortIn, oscPortOut) {
+  socket = io.connect('http://127.0.0.1:8081', {
+    port: 8081,
+    rememberTransport: false
+  });
+  socket.on('connect', function() {
+    socket.emit('config', {
+      server: {
+        port: oscPortIn,
+        host: '127.0.0.1'
+      },
+      client: {
+        port: oscPortOut,
+        host: '127.0.0.1'
+      }
+    });
+  });
+  socket.on('message', function(msg) {
+    if (msg[0] == '#bundle') {
+      for (var i = 2; i < msg.length; i++) {
+        receiveOsc(msg[i][0], msg[i].splice(1));
+      }
+    } else {
+      receiveOsc(msg[0], msg.splice(1));
+    }
+  });
+}
+
+function modelReady() {
+  console.log('model ready');
+}
+
+function drawKeypoints() {
+  // Loop through all the poses detected
+  for (let i = 0; i < min(poses.length, 1); i++) {
+    // For each pose detected, loop through all the keypoints
+    for (let j = 0; j < poses[i].pose.keypoints.length; j++) {
+      // A keypoint is an object describing a body part (like rightArm or leftShoulder)
+      let keypoint = poses[i].pose.keypoints[j];
+      // Only draw an ellipse is the pose probability is bigger than 0.2
+      if (keypoint.score > 0.2) {
+
+        fill(255, 0, 0);
+        noStroke();
+        ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
+
+        poseX = keypoint.position.x;
+        poseY = keypoint.position.y;
+
+        socket.emit('message', [poseX, poseY]);
+      }
+    }
+  }
+}
+
+// A function to draw the skeletons
+function drawSkeleton() {
+  // Loop through all the skeletons detected
+  for (let i = 0; i < poses.length; i++) {
+    let skeleton = poses[i].skeleton;
+    // For every skeleton, loop through all body connections
+    for (let j = 0; j < skeleton.length; j++) {
+      let partA = skeleton[j][0];
+      let partB = skeleton[j][1];
+      stroke(255, 0, 0);
+      line(partA.position.x, partA.position.y, partB.position.x, partB.position.y);
+    }
+  }
+}
